ab46814c00de827349b79a0aa4a2c6e358fa772d
Vincent Rialland <vincent.rialland@gmail.com>
2020-06-07 15:22:02 +0200

    Working example

---
+++ main.py
@@ -0,0 +1,20 @@
+import time
+from pixels import screen
+
+matrix = screen.Screen(32, 16)
+
+matrix.fill(0x222222)
+
+matrix.set_pixel(1, 1, 0xFF0000)
+
+# Rectangle
+matrix.draw_line(0, 0, 31, 0, 0x00FF00)
+matrix.draw_line(0, 15, 31, 15, 0x00FF00)
+matrix.draw_line(0, 0, 0, 15, 0x00FF00)
+matrix.draw_line(31, 0, 31, 15, 0x00FF00)
+# Text
+matrix.draw_text(3, 3, "Hello", 0xFFFFFF)
+
+matrix.draw_circle(16, 8, 4, 0x0000FF)
+
+time.sleep(10)
--- pixels/screen.py
+++ pixels/screen.py
@@ -1,13 +1,15 @@
+import os
+
 from rgbmatrix import RGBMatrix, RGBMatrixOptions, graphics
 
 from .utils import hex2rgb
 
 
 class Screen:
-    def __init__(self, rows, cols, **kwargs):
+    def __init__(self, cols: int, rows: int, **kwargs):
         options = RGBMatrixOptions()
-        options.rows = rows
         options.cols = cols
+        options.rows = rows
         for k, v in kwargs.items():
             setattr(options, k, v)
         self.matrix = RGBMatrix(options=options)
@@ -23,14 +25,15 @@ class Screen:
 
     def draw_circle(self, x: int, y: int, radius: int, color: int):
         color = graphics.Color(*hex2rgb(color))
-        graphics.DrawCircle(x, y, radius, color)
+        graphics.DrawCircle(self.matrix, x, y, radius, color)
 
     def draw_line(self, x1: int, y1: int, x2: int, y2: int, color: int):
         color = graphics.Color(*hex2rgb(color))
-        self.matrix.DrawLine(x1, y1, x2, y2, color)
+        graphics.DrawLine(self.matrix, x1, y1, x2, y2, color)
 
     def draw_text(self, x: int, y: int, text: str, color: int):
         color = graphics.Color(*hex2rgb(color))
         font = graphics.Font()
-        font.LoadFont("../fonts/7x13.bdf")
-        self.matrix.DrawText(self.matrix, font, x, y, color, text)
+        path = os.path.join(os.path.dirname(__file__), "..", "fonts", "7x13.bdf")
+        font.LoadFont(path)
+        graphics.DrawText(self.matrix, font, x, y, color, text)
