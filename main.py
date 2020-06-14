e7bc2423dbf6e2912fac2cc2f9c9a233861ed785
Vincent Rialland <vincent.rialland@gmail.com>
2020-06-14 14:22:10 +0200

    Add server

--- main.py
+++ main.py
@@ -1,22 +1,8 @@
-import time
-from pixels import screen
+import uvicorn
+from pixels.app import app
 
-matrix = screen.Screen(32, 16)
+if __name__ == "__main__":
+    # Serve application
+    import uvicorn
 
-matrix.fill(0x222222)
-
-matrix.set_pixel(1, 1, 0xFF0000)
-
-# Rectangle
-matrix.draw_line(0, 0, 31, 0, 0x00FF00)
-matrix.draw_line(0, 15, 31, 15, 0x00FF00)
-matrix.draw_line(0, 0, 0, 15, 0x00FF00)
-matrix.draw_line(31, 0, 31, 15, 0x00FF00)
-# Text
-matrix.draw_text(3, 3, "Hello", 0xFFFFFF)
-
-matrix.draw_circle(16, 8, 4, 0x0000FF)
-
-matrix.update()
-
-time.sleep(10)
+    uvicorn.run(app, host="0.0.0.0")
